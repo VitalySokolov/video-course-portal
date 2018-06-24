abd7c9436dbd929a75de735a6e5d637a3dd82ab6
Vitalii Sokolov <sokolov.vitaly@gmail.com>
2018-06-24 22:56:48 +0200

    Implemented mockup method for search courses. Implemented mockup method for 'Load More' button. Implemented mockup method for 'Delete' course button.

--- src/app/video-courses/video-courses.module.ts
+++ src/app/video-courses/video-courses.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { CourseListComponent } from './course-list/course-list.component';
 import { CourseItemComponent } from './course-item/course-item.component';
 import { SearchCourseComponent } from './search-course/search-course.component';
+import { FormsModule } from '@angular/forms';
 
 // import { TruncateModule } from 'ng2-truncate';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule
     // TrancateModule
   ],
   declarations: [CourseListComponent, CourseItemComponent, SearchCourseComponent],
