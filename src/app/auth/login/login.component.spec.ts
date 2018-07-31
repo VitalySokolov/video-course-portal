4b4a956f8ebe5885a0a52390715b8814fcd96249
Vitaly Sokolov <sokolov.vitaly@gmail.com>
2018-07-31 11:15:11 +0200

    Move Base href to the Router Module.

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -4,9 +4,6 @@ import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../../material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from '../../app.routing.module';
-import { VideoCoursesModule } from '../../video-courses/video-courses.module';
-import { APP_BASE_HREF } from '@angular/common';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -14,11 +11,10 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [AppRoutingModule, BrowserAnimationsModule, MaterialModule, ReactiveFormsModule, VideoCoursesModule],
-      declarations: [ LoginComponent ],
-      providers: [{provide: APP_BASE_HREF, useValue : '/' }]
+      imports: [BrowserAnimationsModule, MaterialModule, ReactiveFormsModule],
+      declarations: [LoginComponent],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
