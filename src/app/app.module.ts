2bb38c5ba62402dc63b0208e2546cfab9afc1514
Vitaly Sokolov <sokolov.vitaly@gmail.com>
2018-08-01 13:24:47 +0200

    Organize imports.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from '@core/core.module';
 import { VideoCoursesModule } from '@video-courses/video-courses.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthModule } from '@auth/auth.module';
 import { AppRoutingModule } from './app.routing.module';
 
--- src/app/video-courses/search-course/search-course.component.spec.ts
+++ src/app/video-courses/search-course/search-course.component.spec.ts
@@ -1,8 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { SearchCourseComponent } from './search-course.component';
 import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+
+import { SearchCourseComponent } from './search-course.component';
 
 describe('SearchCourseComponent', () => {
   let component: SearchCourseComponent;
